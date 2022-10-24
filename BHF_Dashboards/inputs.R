ac1da6b3cdd54365b8de237a36689069c1ad5b25
Fionna Chalmers <55670266+fionnachalmers@users.noreply.github.com>
2022-10-24 14:04:15 +0100

    Add files via upload
    Uploading initial files

---
+++ BHF_Dashboards/inputs.R
@@ -0,0 +1,6 @@
+nations_options = c("England","Scotland","Wales")
+
+count_options = c("Rows"="N_row","Valid IDs"="N_valid_id","Distinct IDs"="N_distinct_id")
+count_options_selected = "N_distinct_id"
+
+frequency_options = c("Monthly","Weekly")
