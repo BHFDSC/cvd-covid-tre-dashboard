b786db5fb88c738227205023dc8f44ab6c5ef4ea
fionnachalmers <fionnaelizabethchalmers@gmail.com>
2022-10-26 15:44:18 +0100

    replaced files

---
+++ BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/module_appendix.R
@@ -0,0 +1,18 @@
+#Outputs
+appendixOutput <- function(id){
+  ns <- NS(id)
+  tagList(
+      column(12,style=appendix_css,
+       
+       h4("Holding Text"),
+       h5("Sub Title",style="padding-bottom:4px;"),
+       p(span("Some text: ", style = "font-weight: bold;"),
+         "as an example",
+         style=paste0("color:,",colour_bhf_darkred,";margin-bottom:2px;")),
+       p(span("blah blah", style = "font-weight: bold;"),
+         "and blah",
+         style=paste0("color:,",colour_bhf_darkred,";margin-bottom:2px;")),
+      ),
+  )
+}
+
