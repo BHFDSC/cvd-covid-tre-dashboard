71d351af541428b1db6f3218482222e3be45b7c9
fionnachalmers <fionnaelizabethchalmers@gmail.com>
2022-10-26 14:58:24 +0100

    changed sliderinput to update dynamically

--- BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/data.R
+++ BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/data.R
@@ -11,7 +11,7 @@ datasets_available = t.dataset_dashboard %>%
 
 # Test Dataset using GDPPR Date and HES AE
 t.monthly_grouped_gdppr_date = read.csv(paste0(current_dir_data,'/Data/monthly_grouped_gdppr_date.csv')) %>% 
-  mutate(freq="Monthly") %>% rename(date=date_m) 
+  mutate(freq="Monthly") %>% rename(date=date_m)
 t.weekly_grouped_gdppr_date = read.csv(paste0(current_dir_data,'/Data/weekly_grouped_gdppr_date.csv')) %>% 
   mutate(freq="Weekly") %>% rename(date=date_w)
 t.monthly_grouped_hes_ae = read.csv(paste0(current_dir_data,'/Data/monthly_grouped_hes_ae.csv')) %>% 
