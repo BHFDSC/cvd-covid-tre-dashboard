b3e63172795081df14261463f7090e63553c65d1
fionnachalmers <fionnaelizabethchalmers@gmail.com>
2022-11-09 18:02:59 +0000

    reformated whole app - modularised summary

---
+++ BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/R/module_summary_data_description.R
@@ -0,0 +1,35 @@
+dataDescriptionUI <- function(id){
+  ns <- NS(id)
+  tagList(
+    # Outputs ------------------------------------------------------------------
+    textOutput(ns("description"))
+  )
+}
+
+dataDescriptionServer <- function(id, dataset_summary, nation_summary){
+  moduleServer(
+    id,
+    function(input, output, session){
+      
+
+      dataset_desc_filter = reactive({
+        datasets_available %>%
+        filter(.data$Dataset == "gdppr")
+      })
+
+         
+      observeEvent(dataset_summary(),
+      output$description <- renderPrint({
+        #to prevent all descriptions rendering print blank until df filtered
+        if (nrow(dataset_desc_filter())>1){
+          return(cat(""))
+        }
+        cat(dataset_desc_filter() %>%
+          select(Description) %>%
+            pull())
+        })
+      )
+    }
+  )
+}
+
