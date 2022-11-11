326a33b0e460adc2ab3f45a02ef872d88bdba00b
Lars Murdock <lars.murdock@hdruk.ac.uk>
2022-11-11 12:01:40 +0000

    Using fionna's script to incorporate the string limits and hover function into the data dictionary (DT).

--- BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/R/module_summary_data_dictionary.R
+++ BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/R/module_summary_data_dictionary.R
@@ -9,15 +9,44 @@ dataDictionaryServer <- function(id, dataset_summary, nation_summary){
   moduleServer(
     id,
     function(input, output, session){
-      output$tbl = renderDT(
+      
+      data_dict = reactive({
         data_dictionary %>%
-          left_join(datasets_available, by=c("table")) %>%
-          filter(Dataset == dataset_summary())
-        , options = list(lengthChange = FALSE, pageLength = 5), selection = "none"
-      )
+          left_join(select(datasets_available, c("table","Dataset")), by=c("table")) %>%
+          filter(Dataset == dataset_summary()) %>%
+          select(-Dataset,-table,-database)
+      })
+      
+      output$tbl = renderDataTable(
+        
+        data_dict(),
+        
+        selection = "none",
+        rownames = FALSE,
+        
+        options = list(
+          scrollX = TRUE,
+          searching = TRUE,
+          
+          lengthChange = FALSE,
+          pageLength = 5,
+          
+          columnDefs = list(list(
+            targets = "_all",
+            render = JS(
+              "function(data, type, row, meta) {",
+              "return type === 'display' && data != null && data.length > 30 ?",
+              "'<span title=\"' + data + '\">' + data.substr(0, 30) + '...</span>' : data;",
+              "}"
+            )
+            # rowCallback = JS(
+            #   "function(row, data) {",
+            #   "var full_text = 'This rows values are :' + data[0] + ',' + data[1] + '...'",
+            #   "$('td', row).attr('title', full_text);",
+            #   "}")
+          ))
+        )
+      ) 
     }
   )
 }
-
-
-
