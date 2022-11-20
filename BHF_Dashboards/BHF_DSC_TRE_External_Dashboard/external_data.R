05b3e854785fd8f15f0bac213d4aca7a774f87fc
Lars Murdock <lars.murdock@hdruk.ac.uk>
2022-11-20 15:29:16 +0000

    Code changes to read in the scottish dictionary and react to it

--- BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/external_data.R
+++ BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/external_data.R
@@ -10,14 +10,41 @@ datasets_available = t.dataset_dashboard %>%
   filter(!is.na(Title))
 
 # TRE Data Dictionary ----------------------------------------------------------
-# t.data_dictionary = readxl::read_excel((paste0(current_dir_data,
-#                                                '/Data/TRE_DD_391419_j3w9t.xlsx')))
-data_dictionary = read_excel_allsheets('Data/TRE_DD_391419_j3w9t.xlsx',
+
+#England
+t.data_dictionaryEng = read_excel_allsheets('Data/TRE_DD_391419_j3w9t.xlsx',
                                        tibble = FALSE,
                                        except_sheet_no = 1,
                                        skip = 2) %>%
   mutate(table = str_replace(table,paste0("_",database),"")) %>%
   filter(!is.na(table))
+
+# duplicating in case there are any downstream chunks depending on the name 'data_dictionary'
+data_dictionary <- t.data_dictionaryEng
+
+
+#Scotland
+# pathfornow = "C:/Users/LarsMurdock/Documents/Repo/BHF_DSC_HDS/BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/Data/DD_Scotland.xlsx"
+
+t.data_dictionaryScot = read_excel_allsheets( #pathfornow, 
+  "Data/DD_Scotland.xlsx",
+                                       tibble = FALSE,
+                                       except_sheet_no = c(1,2),
+                                       skip = 0,
+                                       collate = FALSE) 
+
+t.data_dictionaryScot = Map(cbind, names(t.data_dictionaryScot), t.data_dictionaryScot) 
+
+# Renaming the tables in the list so that they can be reactively called
+for (i in (1: length(t.data_dictionaryScot))){ colnames(t.data_dictionaryScot[[i]])[1] <- 'table'}
+
+# Merging and removing blank rows  
+t.data_dictionaryScot = t.data_dictionaryScot %>% 
+  dplyr:: bind_rows() %>% 
+  filter( !is.na(Type))
+
+
+
 
 
 # Data Coverage Pre Processed from data_preprocessing
