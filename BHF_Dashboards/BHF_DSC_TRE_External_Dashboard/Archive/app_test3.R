51241e206f741e092e23cd5e05a2177c1c58c801
Fionna Chalmers <55670266+fionnachalmers@users.noreply.github.com>
2022-11-21 14:16:12 +0000

    pushing to main

--- BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/Archive/app_test3.R
+++ BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/Archive/app_test3.R
@@ -1,33 +1,74 @@
 library(shiny)
 
+generateRandomString <- function(n = 10, m = 10) {
+  elements <- c()
+  
+  chars <- c(LETTERS, letters)
+  
+  for(idx in 1:n) {
+    element <- c()
+    
+    for(entry in 1:m) {
+      val <- sample(c("pair","odd"),1)
+      
+      switch(val,
+             pair = {
+               # Add a letter
+               element <- c(element, sample(chars,1))
+             },
+             odd = {
+               # Add a number
+               element <- c(element, as.character(sample(0:9,1)))
+             }
+      )
+    }
+    
+    elements <- c(elements, paste0(element,collapse=""))
+  }
+  
+  elements
+}
+
+# Define the UI
 ui <- fluidPage(
-  shinyjs::useShinyjs(),
-  numericInput("num", "Enter a number", 7),
-  actionButton("submit", "Square that number!"),
-  actionButton("reset", "Reset"),
-  shinyjs::hidden(
-    div(
-      id = "results",
-      h3("The square is"),
-      textOutput("square")
-    )
-  )
+  actionButton("adder", "Add"),
+  tags$div(id = 'placeholder')
 )
 
-server <- function(input, output, session) {
-  output$square <- renderText({
-    input$submit
-    isolate(input$num * input$num)
-  })
+
+# Define the server code
+server <- function(input, output) {
+  rv <- reactiveValues()
   
-  observeEvent(input$reset, {
-    shinyjs::reset("num")
-    shinyjs::hide("results")
-  })
+  rv$counter <- 0
   
-  observeEvent(input$submit, {
-    shinyjs::show("results")
+  observeEvent(input$adder,{
+    rv$counter <- rv$counter + 1
+    
+    add <- sprintf("%03d",rv$counter)
+    
+    prefix <- generateRandomString(1,20)
+    filterId <- paste0(prefix,'_adder_', add)
+    divId <- paste0(prefix,'_adder_div_', add)
+    elementFilterId <- paste0(prefix,'_adder_object_', add)
+    removeFilterId <- paste0(prefix,'_remover_', add)
+    
+    insertUI(
+      selector = '#placeholder',
+      ui = tags$div(
+        id = divId,
+        actionButton(removeFilterId, label = "Remove filter", style = "float: right;"),
+        textInput(elementFilterId, label = "Introduce text", value = "")
+      )
+    )
+    
+    # Observer that removes a filter
+    observeEvent(input[[removeFilterId]],{
+      rv$counter <- rv$counter - 1
+      removeUI(selector = paste0("#", divId))
+    })
   })
 }
 
-shinyApp(ui = ui, server = server)
+# Return a Shiny app object
+shinyApp(ui = ui, server = server, options = list(launch.browser = T))
--- BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/external_data.R
+++ BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/external_data.R
@@ -21,7 +21,41 @@ data_dictionary = read_excel_allsheets('Data/TRE_DD_391419_j3w9t.xlsx',
 
 
 # Data Coverage Pre Processed from data_preprocessing
-t.data_coverage = read_rds("Data/data_coverage")
+#t.data_coverage = read_rds("Data/data_coverage")
+
+t.data_coverage_source = read.csv('Data/export_dashboard_NHSD_20221102_data_coverage.csv')
+
+t.data_coverage = t.data_coverage_source %>% # as.data.frame() %>% 
+  mutate(date_ym = ifelse(date_ym=="", NA, date_ym)) %>%
+  #remove null dates
+  filter(!is.na(date_ym)) %>%
+  separate(date_ym, c("date_y", "date_m"), remove=FALSE, sep = '-') %>%
+  mutate(across(.cols = c(date_y, date_m), .fn = ~ as.numeric(.))) %>%
+  #remove future dates > this year + 1 (to reduce file size)
+  filter(date_y <= (as.numeric(format(Sys.Date(), "%Y"))+1)) %>%
+  #expand to include all dates in between where counts=0
+  group_by(dataset) %>%
+  expand(date_y=min(date_y):max(date_y),date_m=1:12) %>%
+  ungroup() %>%
+  mutate(date_ym = paste0(str_pad(date_y,width=4,pad=0,side="left"),
+                          "-",
+                          str_pad(date_m,width=2,pad=0,side="left"))) %>%
+  left_join(t.data_coverage_source, by = c("date_ym","dataset")) %>%
+  mutate(across(.cols = starts_with('n'),
+                .fn = ~ replace_na(.,0))) %>%
+  group_by(dataset) %>%
+  arrange(dataset,date_y,date_m) %>%
+  mutate(across(.cols = starts_with("n"),
+                .names = "{.col}_cum",
+                .fn = ~ cumsum(.))) %>%
+  filter(!if_all(ends_with("cum"), ~ . == 0)) %>%
+  select(!ends_with("cum")) %>%
+  #date month names for plot annotation
+  mutate(date_name = paste0(month.name[date_m]," ", date_y, ": ")) %>%
+  mutate(date_name_season = paste0(date_y, ": ")) %>%
+  mutate(date_m_name = paste0(month.name[date_m])) %>%
+  pivot_longer(cols=starts_with("n"), names_to="Type",values_to="N") %>%
+  mutate(date_format = as.Date(paste(date_ym, 1, sep="-"), "%Y-%m-%d"))
 
 # Dataset Overview -------------------------------------------------------------
 t.dataset_overview = read.csv('Data/export_dashboard_NHSD_20221108_date_overview.csv')
