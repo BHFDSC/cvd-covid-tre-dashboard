1aef4e937bf5e71e180e9610ade8e16e73987038
Fionna Chalmers <55670266+fionnachalmers@users.noreply.github.com>
2022-12-16 14:29:43 +0000

    coverage code can now handle welsh data also added a wales linkage matching file

--- BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/external_data.R
+++ BHF_Dashboards/BHF_DSC_TRE_External_Dashboard/external_data.R
@@ -12,7 +12,7 @@ overview_dataset_name_england = "export_dashboard_NHSD_20221108_date_overview"
 export_date_scotland = "2022-11-26"
 
 #Wales
-export_date_wales = "2022-12-16"
+export_date_wales = "2022-11-30"
 coverage_dataset_name_wales = "export_dashboard_SAIL_20221130_data_coverage"
 
 
@@ -87,23 +87,30 @@ t.data_dictionaryWales = read_excel_allsheets( # pathfornow,
 # Data Coverage Pre Processed from data_preprocessing
 #t.data_coverage = read_rds("Data/data_coverage")
 
-t.data_coverage_source = read.csv(paste0('Data/',coverage_dataset_name_england,'.csv'))
 
-t.data_coverage = t.data_coverage_source %>% # as.data.frame() %>% 
-  mutate(date_ym = ifelse(date_ym=="", NA, date_ym)) %>%
+t.data_coverage_source = read.csv(paste0('Data/',coverage_dataset_name_england,'.csv')) %>%
+  rbind(read.csv(paste0('Data/',coverage_dataset_name_wales,'.csv')) %>% rename(n_id_distinct=n_distinct)) %>%
+  as.data.frame() %>%   
+  mutate(date_ym_reformat = ifelse(str_detect(date_ym,"-"),1,ifelse(date_ym=="","",0))) %>%
+  mutate(date_ym = ifelse(date_ym_reformat==0,str_pad(date_ym,6,side="left",pad=0),date_ym)) %>%
+  mutate(date_ym = ifelse(date_ym_reformat==0,paste0(substr(date_ym,1,4),"-",substr(date_ym,5,6)),date_ym)) %>%
+  select(-date_ym_reformat)
+
+t.data_coverage = t.data_coverage_source %>%
+  mutate(date_ym = ifelse(date_ym=="", NA, date_ym)) %>% 
   #remove null dates
-  filter(!is.na(date_ym)) %>%
-  separate(date_ym, c("date_y", "date_m"), remove=FALSE, sep = '-') %>%
-  mutate(across(.cols = c(date_y, date_m), .fn = ~ as.numeric(.))) %>%
+  filter(!is.na(date_ym)) %>% 
+  separate(date_ym, c("date_y", "date_m"), remove=FALSE, sep = '-') %>% 
+  mutate(across(.cols = c(date_y, date_m), .fn = ~ as.numeric(.))) %>% 
   #remove future dates > this year + 1 (to reduce file size)
-  filter(date_y <= (as.numeric(format(Sys.Date(), "%Y"))+1)) %>%
+  filter(date_y <= (as.numeric(format(Sys.Date(), "%Y"))+1)) %>% 
   #expand to include all dates in between where counts=0
   group_by(dataset) %>%
   expand(date_y=min(date_y):max(date_y),date_m=1:12) %>%
   ungroup() %>%
   mutate(date_ym = paste0(str_pad(date_y,width=4,pad=0,side="left"),
                           "-",
-                          str_pad(date_m,width=2,pad=0,side="left"))) %>%
+                          str_pad(date_m,width=2,pad=0,side="left"))) %>% 
   left_join(t.data_coverage_source, by = c("date_ym","dataset")) %>%
   mutate(across(.cols = starts_with('n'),
                 .fn = ~ replace_na(.,0))) %>%
@@ -111,8 +118,8 @@ t.data_coverage = t.data_coverage_source %>% # as.data.frame() %>%
   arrange(dataset,date_y,date_m) %>%
   mutate(across(.cols = starts_with("n"),
                 .names = "{.col}_cum",
-                .fn = ~ cumsum(.))) %>%
-  filter(!if_all(ends_with("cum"), ~ . == 0)) %>%
+                .fn = ~ cumsum(.))) %>% 
+  filter(!if_all(ends_with("cum"), ~ . == 0)) %>% 
   select(!ends_with("cum")) %>%
   #date month names for plot annotation
   mutate(date_name = paste0(month.name[date_m]," ", date_y, ": ")) %>%
@@ -120,6 +127,8 @@ t.data_coverage = t.data_coverage_source %>% # as.data.frame() %>%
   mutate(date_m_name = paste0(month.name[date_m])) %>%
   pivot_longer(cols=starts_with("n"), names_to="Type",values_to="N") %>%
   mutate(date_format = as.Date(paste(date_ym, 1, sep="-"), "%Y-%m-%d"))
+
+
 
 # Dataset Overview -------------------------------------------------------------
 t.dataset_overview = read.csv(paste0('Data/',overview_dataset_name_england,'.csv'))
