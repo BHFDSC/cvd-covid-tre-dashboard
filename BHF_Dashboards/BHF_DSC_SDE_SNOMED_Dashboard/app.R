b06b06f19e195949b2f306780111e002f5cd80d7
favour <fyaccfavour@gmail.com>
2023-07-31 13:05:03 +0100

    updating tables

--- BHF_Dashboards/BHF_DSC_SDE_SNOMED_Dashboard/app.R
+++ BHF_Dashboards/BHF_DSC_SDE_SNOMED_Dashboard/app.R
@@ -13,29 +13,20 @@ library(reactable)
 library(viridis)
 options(scipen=99999)
 
-#df <- read_csv('full_df.csv')
-df <- read_csv("table2.csv")
-`Cluster Descriptions` <- read_csv("table1.csv")
-#final <- read_csv('full_df.csv')
+df2 <- read_csv('full_df.csv')
 dict <- read_excel('data dictionary.xlsx')
 
-
-sum(is.na(df$ConceptId_2))
-df$date_ym <- anydate(df$date_ym)
-df$date_ym <- ymd(df$date_ym)
-df <- replace(df, is.na(df), 0)
-
-cluster_desc <- read_csv('cluster_desc.csv')
-df <- merge(df, cluster_desc, by = "Cluster_ID", all.x = TRUE)
-
-
+sum(is.na(df2$ConceptId_2))
+df2$date_ym <- anydate(df2$date_ym)
+df2$date_ym <- ydm(df2$date_ym)
+df2 <- replace(df2, is.na(df2), 0)
 
 #add comma to numeric columns for cluster and timeseries
-`Cluster Descriptions`$n <- format(`Cluster Descriptions`$n, big.mark = ",")
-`Cluster Descriptions`$n_id_distinct <- format(`Cluster Descriptions`$n_id_distinct, big.mark = ",")
+df2$records_total <- format(df2$records_total, big.mark = ",")
+df2$individuals <- format(df2$individuals, big.mark = ",")
 
-#separate into month and year for dendro
-`Data Tree` <- separate(df, date_ym, into=c('year', 'month'), sep = '-')
+#separate into month and year for dendrogram
+`Data Tree` <- separate(df2, date_ym, into=c('year', 'month'), sep = '-')
 #remove all \
 `Data Tree`$ConceptId_Description_2 <- gsub("\\([^\\)]+\\)|\\[[^\\]]+\\]", "", `Data Tree`$ConceptId_Description_2)
 #remove all :
@@ -52,8 +43,8 @@ ui <- dashboardPage(
       tabItem(
         tabName = 'page1',
         fluidRow(
-          column(width = 12, 
-                 imageOutput("pic", height = "auto", width = "auto"))
+          #column(width = 12, 
+          imageOutput("pic", height = "auto", width = "auto")
         ),
         fluidRow(
           column(width = 12, box(title = 'Overview', height = 250, width = 300, textOutput('overviewtext'))),
@@ -100,13 +91,16 @@ ui <- dashboardPage(
                                         actionButton('resize', 'Click to view cases above 1000', width = '200px'),
                                         actionButton('reset', 'Click to Reset Plot'),
                                         actionButton('earlydates', 'Click to View percentage before 1990'),
-                                        ))),
+                                        checkboxInput('log',"Tick to view Log Scale", F)
+        ))),
         
         fluidRow(column(width = 12, box(width = 500, title = 'Timeseries', 
                                         solidHeader = TRUE, status = 'success',
                                         plotlyOutput('timeseries', height = 300),
                                         conditionalPanel(condition = 'input.compare',
-                                                         plotlyOutput('timeseriestwo', height = 300)))))
+                                                         plotlyOutput('timeseriestwo', height = 300)),
+                                        conditionalPanel(condition = 'input.log', 
+                                                         plotlyOutput('timeseriesthree', height = 300)))))
       )
     )
   )
@@ -114,12 +108,14 @@ ui <- dashboardPage(
 
 server <- function(input, output, session) {
   session$allowReconnect(TRUE)
-  filtered <- reactive({`Cluster Descriptions`[`Cluster Descriptions`$Cluster_Desc == input$data, ]})
-  filteredtwo <- reactive({`Cluster Descriptions`[`Cluster Descriptions`$Cluster_Desc == input$otherdata, ]})
+  filtered <- reactive({df2[df2$Cluster_Desc == input$data, ]})
+  filteredtwo <- reactive({df2[df2$Cluster_Desc == input$otherdata, ]})
+  filteredval <- reactive({df2[df2$Cluster_Desc == input$data, ][1, ]})
   
   output$pic <- renderImage({
-    return(list(src = "www/Heart_data_title_slide.jpg", contentType = "image/jpeg", width = "100%", height = 600))
+    return(list(src = "www/Heart_data_title_slide.jpg", contentType = "image/jpeg", width = "100%", height = 500))
   }, deleteFile = FALSE) #where the src is wherever you have the picture
+  
   
   output$overviewtext <- renderText({'Welcome to this interactive Dashboard! 
   The Dashboard provides information on certain groups of patients, 
@@ -130,10 +126,11 @@ server <- function(input, output, session) {
   of Medicine - Clinical Terms. That is, 
   it is a system used to standardise the description of clinical terms
     in Hospitals and GP practices.'
-    })
+  })
   
-  output$snomed <- renderText({'SNOMED CT stands for Systemised Nomencleture 
-  of Medicine - Clinical Terms. That is, 
+  
+  output$snomed <- renderText({'SNOMED CT stands for Systemised Nomencleture
+  of Medicine - Clinical Terms. That is,
   it is a system used to standardise the description of clinical terms
     in Hospitals and GP practices.'})
   
@@ -142,43 +139,44 @@ server <- function(input, output, session) {
   })
   
   
+  
   output$dictionary <- DT::renderDataTable(dict, options = list(pageLength = 5))
   
   output$tree <- renderCollapsibleTree({
-    collapsibleTree(`Data Tree`, c('Cluster_ID', 'year', 'month', 'ConceptId_Description_2'), 
+    collapsibleTree(`DataTree`, c('Cluster_ID', 'year', 'month', 'ConceptId_Description_2'),
                     tooltip = TRUE, attribute = 'n')
   })
   
   output$cluster <- renderCollapsibleTree({
-    collapsibleTree(`Cluster Descriptions`, 
-                    c('Cluster_Category', 'Cluster_Desc', 'Cluster_ID'), 
+    collapsibleTree(df2,
+                    c('Cluster_Category', 'Cluster_Desc', 'Cluster_ID'),
                     tooltip = TRUE, attribute = 'n_code')
   })
   
   
   output$clustername <- renderValueBox({
-    value <- filtered()$Cluster_ID
+    value <- filteredval()$Cluster_ID
     valueBox(subtitle = "Cluster ID", value = value)
   })
   
   output$clustercat <- renderValueBox({
-    valueBox(subtitle = "Cluster Category", value = filtered()$Cluster_Category)})
+    valueBox(subtitle = "Cluster Category", value = filteredval()$Cluster_Category)})
   
   
   output$clustern <- renderValueBox({
-    value <- filtered()$n
+    value <- filteredval()$records_total
     valueBox(subtitle = "Records in GDPPR", value = value)
   })
   
   output$individuals <- renderValueBox({
-    value <- filtered()$n_id_distinct
+    value <- filteredval()$individuals
     valueBox(subtitle = "Distinct Individuals", value = value)
   })
   
   output$clusterdesc <- renderValueBox({
-    valueBox(subtitle = "Cluster Description", value = filtered()$Cluster_Desc)})
+    valueBox(subtitle = "Cluster Description", value = filteredval()$Cluster_Desc)})
   
-  observeEvent(input$earlydates, {shinyalert(text = filtered()$pre_1990_n_pct, 
+  observeEvent(input$earlydates, {shinyalert(text = filteredval()$pre_1990_n_pct,
                                              animation = T, type = 'info')})
   
   filtered_data <- reactiveVal()
@@ -186,8 +184,8 @@ server <- function(input, output, session) {
   
   
   observeEvent(input$resize, {
-    filtered_data(df[df$Cluster_ID %in% filtered() & df$n > 1000, ])
-    filtered_datatwo(df[df$Cluster_ID %in% filteredtwo() & df$n > 1000, ])
+    filtered_data(filtered()[filtered()$records_month > 1000, ]) #(df2[df2$Cluster_ID %in% filtered() & df2$records_month > 1000, ])
+    filtered_datatwo(filteredtwo()[filteredtwo()$records_month > 1000, ])
   })
   
   
@@ -196,9 +194,14 @@ server <- function(input, output, session) {
     filtered_datatwo(NULL)
   })
   
+  observeEvent(input$data, {
+    filtered_data(NULL)
+  })
+  
+  
   
   output$timeseries <- renderPlotly({
-    filtered_plot_data <- df[df$Cluster_ID %in% filtered(), ]
+    filtered_plot_data <- filtered() #df2[df2$Cluster_ID %in% filtered(), ]
     
     if (is.null(filtered_data())) {
       plot_data <- filtered_plot_data
@@ -207,10 +210,10 @@ server <- function(input, output, session) {
     }
     
     ggplotly(
-      ggplot(plot_data, aes(x = date_ym, fill = ConceptId_Description_2)) + 
-        geom_area(aes(y = n, fill = ConceptId_Description_2)) +
+      ggplot(plot_data, aes(x = date_ym, fill = ConceptId_Description_2)) +
+        geom_area(aes(y = records_month, fill = ConceptId_Description_2)) +
         #geom_line(aes(y=n), alpha = 0.7) +
-        labs(x = 'Date', y='Cases') + 
+        labs(x = 'Date', y='Cases') +
         scale_y_continuous(labels = scales::comma) +
         scale_fill_viridis_d() +
         #scale_y_log10(labels = scales::comma) +
@@ -222,7 +225,7 @@ server <- function(input, output, session) {
   
   output$timeseriestwo <- renderPlotly({
     req(input$compare)
-    filtered_plot_datatwo <- df[df$Cluster_ID %in% filteredtwo(), ]
+    filtered_plot_datatwo <- filteredtwo() #df[df$Cluster_ID %in% filteredtwo(), ]
     
     if (is.null(filtered_datatwo())) {
       plot_data <- filtered_plot_datatwo
@@ -232,7 +235,7 @@ server <- function(input, output, session) {
     
     ggplotly(
       ggplot(plot_data, aes(x = date_ym, fill = ConceptId_Description_2)) + 
-        geom_area(aes(y = n, fill = ConceptId_Description_2)) +
+        geom_area(aes(y = records_month, fill = ConceptId_Description_2)) +
         #geom_line(aes(y=n), alpha = 0.7) +
         labs(x = 'Date', y='Cases') + 
         scale_y_continuous(labels = scales::comma) +
@@ -244,48 +247,31 @@ server <- function(input, output, session) {
     
   })
   
+  output$timeseriesthree <- renderPlotly({
+    req(input$log)
+    filtered_plot_datatwo <- filteredtwo() #df[df$Cluster_ID %in% filteredtwo(), ]
+    
+    if (is.null(filtered_datatwo())) {
+      plot_data <- filtered_plot_datatwo
+    } else {
+      plot_data <- filtered_datatwo()
+    }
+    
+    ggplotly(
+      ggplot(plot_data, aes(x = date_ym, colour = ConceptId_Description_2)) + 
+        #geom_area(aes(y = n, fill = ConceptId_Description_2)) +
+        geom_line(aes(y=records_month), alpha = 0.7) +
+        labs(x = 'Date', y='Cases') + 
+        #scale_y_continuous(labels = scales::comma) +
+        scale_fill_viridis_d() +
+        scale_y_log10(labels = scales::comma) +
+        theme(legend.position = 'none'), tooltip = 'colour'
+    ) %>% layout(plot_bgcolor = "white",
+                 paper_bgcolor = "white")
+    
+  })
+  
+  
 }
 
 shinyApp(ui, server)
-
-
-#ggplotly(
-#  ggplot(df[df$Cluster_ID == 'ANTIHYP_COD', ], aes(x = date_ym, fill = ConceptId_Description_2)) + 
-#    geom_area(aes(y = n, fill = ConceptId_Description_2)) +
-    #geom_line(aes(y=records_month), alpha = 0.7) +
-#    labs(x = 'Date', y='Cases') + 
-#    scale_y_continuous(labels = scales::comma) +
-#    scale_fill_viridis_d() +
-    #scale_y_log10(labels = scales::comma) +
-#    theme(legend.position = 'none'), tooltip = 'fill'
-#) %>% layout(plot_bgcolor = "white",
-#             paper_bgcolor = "white")
-
-
-# header <- dashboardHeader()
-# sidebar <- dashboardSidebar(
-#   sidebarMenu(id = "test",
-#               menuItem("Alignment", tabname = "AlignmentTab")
-#   )
-# )
-# body <- dashboardBody(
-#   tabItem(tabName = "AlignmentTab",
-#           fluidRow(column(width = 12,
-#             
-#               imageOutput("Alignment", height = "auto", width = "auto")
-#             
-#           ))
-#   )
-# )
-# 
-# ui <- dashboardPage(header, sidebar, body)
-# server <- function(input, output, session) {
-#   
-#   output$Alignment <- renderImage({
-#     return(list(src = "www/Heart_data_title_slide.jpg",alt = "Alignment"))
-#   }, deleteFile = FALSE) #where the src is wherever you have the picture
-#   
-# }
-# 
-# #Combines Dasboard and Data together----
-# shinyApp(ui, server)
