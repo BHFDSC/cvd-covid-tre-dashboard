66e56813d7fa98b28ef6051025d603fa3d8ed7fb
Fionna Chalmers <55670266+fionnachalmers@users.noreply.github.com>
2023-09-18 23:11:17 +0100

    restrucutred files

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# BHF_DSC_HDS
+# BHF Data Science Centre TRE Exploration Dashboard
 
-## Description
+Accredited researchers, working on one or more of the consortium’s approved projects, can access routinely collected datasets across the whole population of the UK within secure trusted research environments (TREs) provided by NHS Digital in England, the National Data Safe Haven in Scotland, the SAIL Databank in Wales and the Honest Broker Service in Northern Ireland.
 
-Repo for the BHF Data Science Centre's Health Data Science team to share code
+Linkable datasets include those from primary and secondary care, COVID lab tests and vaccinations, deaths, critical care, prescribing/dispensing, cardiovascular and stroke audits, maternity services and mental health.
+
+This dashboard exists to allow exploration of the datasets available to the consortium in the respective TREs.
+
+---
+
+### Application Structure
---
+++ app.R
@@ -0,0 +1,250 @@
+# Setup ########################################################################
+Version = "1.1.0"
+
+options(shiny.sanitize.errors = TRUE)
+
+#Modules scripts are sourced from the R folder
+library(shiny)
+library(bslib)
+library(tidyverse)
+library(shinydashboard)
+library(shinyWidgets)
+library(shinyBS)
+library(shinyjs)
+library(hover)
+library(DT)
+library(scales)
+library(ggiraph)
+library(patchwork)
+library(fontawesome)
+library(ggtext)
+library(prompter)
+library(readxl)
+library(ggrepel)
+library(htmltools)
+library(bsplus)
+library(showtext)
+library(spsComps)
+library(shinyalert)
+library(reactable)
+library(waiter)
+library(shinycssloaders)
+library(toOrdinal)
+library(sass)
+
+library(tippy)
+
+#External Sources
+source('external_common_functions.R')
+source('external_bhf_dsc_hds_designkit.R')
+source('external_data.R')
+source('external_inputs.R')
+
+## change path to a non temp directory to keep that even after reboot
+# shinyOptions(cache = cachem::cache_disk(file.path(dirname(tempdir()), 
+#                                                   "myapp-cache")))
+
+#shinyOptions(cache = cachem::cache_disk("./bind-cache"))
+# User Interface ---------------------------------------------------------------
+
+
+ui = fluidPage(
+  
+  #tags$head(HTML("<title>BHF DSC Dashboard</title>")), #window title on the browser
+  tags$head(HTML("<title>BHF DSC Dashboard</title> <link rel='icon' type='image/png' sizes = '32x32' href='/browser_logo.png'>")),
+  
+
+  ## UI Setup and Design #######################################################
+  shinyjs::useShinyjs(),
+  # This creates a google analytics tag, which allows traffic to be tracked once deployed
+  # see https://shiny.posit.co/r/articles/build/google-analytics/ for more instructions
+  #tags$head(includeHTML(("google-analytics.html"))),
+  #App Theme
+  theme = bhf_dsc_hds_bootstrap_theme,
+  # Load the dependencies for prompter
+  use_prompt(),
+  #CSS Theme Overrides
+  tags$head(tags$style(HTML(bhf_dsc_hds_css))),
+  #JS Script https://shiny.rstudio.com/articles/packaging-javascript.html
+  tags$head(tags$script(src="bhf_dsc_design.js")),
+  tags$head(tags$script(src="shinyLink.js")),
+
+  useWaitress(),
+
+
+
+  #SCSS code converted to css using sass (message silenced)
+  capture.output(
+    (sass::sass(
+    input = list(
+      sass::sass_file("www/bhf_dsc_design.scss")
+    ),
+    output = "www/bhf_dsc_design_sass.css",
+    options = sass::sass_options(output_style = "compressed")
+  )),file='NUL'),
+  #Then read in from www
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "bhf_dsc_design_sass.css")
+  ),
+
+  
+
+
+  bsplus::use_bs_tooltip(),
+  bsplus::use_bs_popover(),
+  
+  
+
+
+  div(
+  id = "main_content",
+  ## Navigation Bar ############################################################
+  navbarPage(id="navmain",
+    position = "fixed-top",
+    #bhf_navbar_line,
+    #BHF HDS Logo
+    
+    
+    
+    title = tags$a(img(src = "bhf_dsc_logo.png",
+                    style = "margin-top:2% !important;
+                        justify-content: center !important;
+                        align-items: center !important;",
+                    height = 70),
+                   target = "_blank", #open in new tab
+                   href="http://bhfdatasciencecentre.org/"),
+    
+    
+    ### About Tab ===========================================================
+    tabPanel("About",value = "aboutdefault", aboutUI(id = "about_module")),
+
+    ### Summary Tab ============================================================
+  
+    tabPanel(HTML('<span style=""><center>Dataset Summary</center></span>'), value = "summary", summaryUI(id = "summary_module")),
+    
+    ### Compare Tab ============================================================
+    tabPanel(HTML('<span style=""><center>Dataset Comparison</center></span>'),
+             value = "comparing",
+             #compareUI(id = "compare_module")
+             source("source_compareUI.R",local = TRUE)$value
+             ),
+    
+    ### Insight Tab ============================================================
+    #tabPanel("Insight"),
+    
+    ### Methodology Tab ===========================================================
+    tabPanel("Methodology",methodologyOutput(id = "methodology")) ,
+    
+    ### Feedback Button ===========================================================
+    
+    # tabPanel(actionBttn(inputId='ab1', label="Provide Feedback", color = "success", 
+    #                     position = "right",
+    #                     # icon = icon("th"), 
+    #                     # size = lg ,
+    #                     style = "unite"  , 
+    #                     onclick ="window.open('https://forms.office.com/Pages/ResponsePage.aspx?id=saxMhCdwY0Kdihj6pb8IOe1OSlBUSpBCq1NpVakPfMJURTY0TTZCRFlHOUEyMTM3QUJWTkxFR1AwUC4u',)"))
+    # 
+
+    )
+  ),
+    
+    ## Footer ==================================================================
+
+    #hr(),
+    tags$footer(div(
+      class = "footer",
+      #includeHTML('footer.html') #now archived
+      HTML(footer_template(export_date = paste(export_date_england,export_date_scotland,export_date_wales),
+                           email_link = "mailto:bhfdsc@hdruk.ac.uk",
+                           twitter_link = "https://twitter.com/HDR_UK",
+                           youtube_link = "https://www.youtube.com/playlist?list=PLBI5k9SgYrItGXrJo3wO2LtsxwfyvimZ5"
+                           ))
+))
+  
+
+
+)
+
+# Server -----------------------------------------------------------------------
+
+server = function(input, output, session) {
+
+  # observeEvent(input$navmain, {
+  #   if(input$navmain == "About"){
+  #     addCssClass(class = "abouttab", selector = ".navbar")
+  #     removeCssClass(class = "normaltab", selector = ".navbar")
+  #     #addCssClass(class = "abouttab", selector = ".container-fluid")
+  #    # removeCssClass(class = "normaltab", selector = ".container-fluid")
+  #   } else {
+  #     addCssClass(class = "normaltab", selector = ".navbar")
+  #     removeCssClass(class = "abouttab", selector = ".navbar")
+  #     #addCssClass(class = "normaltab", selector = ".container-fluid")
+  #     #removeCssClass(class = "abouttab", selector = ".container-fluid")
+  #   }
+  # })
+
+  
+  observe({
+    if(input$navmain == "aboutdefault"){
+      addCssClass(class = "abouttab", selector = ".navbar")
+      removeCssClass(class = "normaltab", selector = ".navbar")
+      #addCssClass(class = "abouttab", selector = ".container-fluid")
+      # removeCssClass(class = "normaltab", selector = ".container-fluid")
+    } else {
+      addCssClass(class = "normaltab", selector = ".navbar")
+      removeCssClass(class = "abouttab", selector = ".navbar")
+      #addCssClass(class = "normaltab", selector = ".container-fluid")
+      #removeCssClass(class = "abouttab", selector = ".container-fluid")
+    }
+  })
+  
+  
+  # observeEvent(input$testid,{
+  #   hide("loading_page", anim = TRUE, animType = "fade", time = 0.4)
+  #   delay(ms = 600, show("main_content", anim = TRUE, animType = "fade", time = 3))
+  # })
+  
+  #observe(print(outputOptions(output)))
+  #was attempting to load data in background whilst on load page
+  #outputOptions(output, "summary_module-data_overview-registrations", suspendWhenHidden = FALSE)
+  
+  # # call the waitress
+  # waitress <- Waitress$
+  #   new(theme = "overlay-percent")$
+  #   start() # start
+  # 
+  # for(i in 1:10){
+  #   waitress$inc(10) # increase by 10%
+  #   Sys.sleep(.3)
+  # }
+  # 
+  # # hide when it's done
+  # waitress$close() 
+  
+  # observeEvent(input$myButton, {
+  #   runjs(paste0('$("#ab1").css("animation","")'))
+  # })
+
+
+  aboutServer(id = "about_module")
+  ### Summary Tab ============================================================
+  summaryServer(id = "summary_module")
+  
+  ### Compare Tab ============================================================
+  #compareServer(id = "compare_module")
+  source("source_compareServer.R",local = TRUE)$value
+  
+  ### Insight Tab ============================================================
+  
+  ### Methodology Tab ===========================================================
+
+
+  
+}
+
+
+# Run the application ----------------------------------------------------------
+shinyApp(ui = ui, server = server)
+#runGadget(ui, server, viewer = browserViewer(browser = getOption("browser")))
+
+
